dbaa2e88f8327350cf102316b2956d8cdef65f62
Тимур <42223074+TimurMamdov1991@users.noreply.github.com>
2021-06-23 20:38:13 +0300

    CI Jenkins

--- src/main/java/homework_number/task_2/Aircraft/Cargo/CargoGyrocopterPlane.java
+++
@@ -1,9 +0,0 @@
-package homework_number.task_2.Aircraft.Cargo;
-
-public  class CargoGyrocopterPlane extends CargoPlane {
-
-
-  public CargoGyrocopterPlane(String name, double range, double cargo) {
-    super (name, range, cargo);
-  }
-}
--- src/main/java/homework_number/task_2/Aircraft/Passenger/PassengerHelicopterPlane.java
+++
@@ -1,7 +0,0 @@
-package homework_number.task_2.Aircraft.Passenger;
-
-public class PassengerHelicopterPlane extends PassengerPlane {
-  public PassengerHelicopterPlane(String name, double range, double roominess, double cargo) {
-    super (name, range, roominess, cargo);
-  }
-}
